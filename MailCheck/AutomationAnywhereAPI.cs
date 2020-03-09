63c65de3bbac79f802f8a0ca932499341d79f31d
Thomas <leyseth@cronos.be>
2020-03-09 14:22:07 +0100

    v0.1
    Authentication Post added

--- MailCheck/AutomationAnywhereAPI.cs
+++ MailCheck/AutomationAnywhereAPI.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MailCheck
@@ -56,9 +58,26 @@ namespace MailCheck
             Console.WriteLine(response.Content.ReadAsStringAsync().Result);
         }
 
-        public async void Authorize(string url)
+        public void Authorize(string url)
         {
-            
+            String querystring = "{\"username': 'string', 'password': 'string', 'apiKey': 'string', 'mfaCode': 0}";
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = "{\"username\":\"aa_leyseth\",\"password\": \"gismo2340\", \"apiKey\": \"eu,Tkrd&+)/&UWcMava5V=DVh?6(!08^/w7WAamf\", \"mfaCode\": 0}";
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                authb = streamReader.ReadToEnd();
+            }
             
         }
     }
