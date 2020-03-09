57ed94de1debb98302a3f8540368254ddfbac321
Thomas <leyseth@cronos.be>
2020-03-09 13:16:51 +0100

    v0.02
    Succesful test API get/post

---
+++ MailCheck/AutomationAnywhereAPI.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailCheck
+{
+    class AutomationAnywhereAPI
+    {
+        HttpClient client = new HttpClient();
+        private string responseb;
+
+        private string authb;
+
+        public string authbody
+        {
+            get { return authb; }
+            set { authb = value; }
+        }
+
+        public string responsebody {
+            get { return responseb; }
+            set { responseb = value; } 
+        }
+
+
+        public async void getRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        responseb = await content.ReadAsStringAsync();
+                    }
+                }                
+            }
+        }
+
+
+        public async void postRequest(string url)
+        {
+            IEnumerable<KeyValuePair<string, string>> queries = new List<KeyValuePair<String, String>>()
+            {
+                new KeyValuePair<string, string>("test", "testest"),
+            };
+            HttpContent postcontent = new FormUrlEncodedContent(queries);
+            var response = client.PostAsync(url, postcontent).Result;
+            if (response.StatusCode == HttpStatusCode.InternalServerError)
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+        }
+
+        public async void Authorize(string url)
+        {
+            
+            IEnumerable<KeyValuePair<string, string>> queries = new List<KeyValuePair<String, String>>()
+            {
+                new KeyValuePair<string, string>("username", "aa_leyseth"),
+                new KeyValuePair<string, string>("password", "gismo2340"),
+                new KeyValuePair<string, string>("apiKey", "#2K[(Ya['YFm`CfzH6 |&4+d$w={*2Q27GvqjLd2"),
+                new KeyValuePair<string, string>("mfaCode", "0"),
+            };
+            HttpContent postcontent = new FormUrlEncodedContent(queries);
+            var response = client.PostAsync(url, postcontent).Result;
+            authb = response.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
--- MailCheck/Program.cs
+++ MailCheck/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace MailCheck
 {
@@ -6,8 +7,23 @@ namespace MailCheck
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().Wait();
+        }
 
+        static async Task MainAsync()
+        {
+            try
+            {
+                AutomationAnywhereAPI aaapi = new AutomationAnywhereAPI();
+                //aaapi.getRequest("http://www.google.com");
+                aaapi.Authorize("http://cbkonw-dyn-aa.iconos.be/v1/authentication");
+                //aaapi.postRequest("http://ptsv2.com/t/s7oqt-1583748337/post");
+                Console.WriteLine(aaapi.authbody);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
